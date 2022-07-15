cf7bba20e0445018b84956dc4f4cb604eefb81a5
JenniBeadle <jenni_beadle@sil.org>
2022-07-15 17:08:01 +0800

    Deploy website - based on 49d9c214da8be9da7628cc9c65576c61472d0302

---
+++ assets/js/af6e0c10.886bb465.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkparatextmanual=self.webpackChunkparatextmanual||[]).push([[373],{5745:function(a){a.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ assets/js/de8cfe0f.028fd1ca.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkparatextmanual=self.webpackChunkparatextmanual||[]).push([[277],{3769:function(a){a.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
